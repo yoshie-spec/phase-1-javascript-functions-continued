a09427cfc1971205e2900d9c20ce26614af29f77
yosselin alvarez <yoshie_9311@icloud.com>
2023-04-09 01:03:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("run");
+
+
+
+function mondayWork(must= "go to the office"){
+return `This Monday, I will ${must}.`;
+}
+mondayWork();
+mondayWork('work from home.');
+
+function wrapAdjective(parameter= `*`) {
+    const innerFunction= function (string=`special`){
+        return `You are ${parameter}${string}${parameter}!`;
+    };
+    return innerFunction;
+}
+
+wrapAdjective("%")("a dedicated programmer");
+
+
+
+
+
+
+
+
+
+
+
+// it("when initialized with '*' creates a function that, when called, wraps an adjective in a highlight", function() {
+//     let result = wrapAdjective('*')
+//     let emphatic = result("a hard worker")
+//     expect(emphatic).to.equal("You are *a hard worker*!")
+//   });
+
+//   it("when initialized with '||' creates a function that, when called, wraps an adjective in a highlight", function() {
+//     let result = wrapAdjective("||")
+//     let emphatic = result("a dedicated programmer")
+//     expect(emphatic).to.equal("You are ||a dedicated programmer||!")
+//   });
